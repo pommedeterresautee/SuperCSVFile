d9ec2b7b4a8b1e7a96eaf051bca98baca78b4d80
El Potaeto <contact@2borange.com>
2014-03-21 18:28:41 +0100

    Add tests for fake encoding

---
+++ src/main/scala/com/taj/supertaxlawyer/CommandLineParser.scala
@@ -0,0 +1,40 @@
+package com.taj.supertaxlawyer
+
+import org.rogach.scallop.ScallopConf
+import java.io.File
+
+
+object CommandLineParser {
+  def apply (args: Array[String]) = new CommandLineParser(args)
+}
+
+/**
+ * Parser of command line arguments.
+ * @param args arguments provided through the command line.
+ */
+class CommandLineParser(args: Array[String]) extends ScallopConf(args) {
+    banner( """
+                  | ____                          _____            _
+                  |/ ___| _   _ _ __   ___ _ __  |_   _|_ ___  __ | |    __ ___      ___   _  ___ _ __
+                  |\___ \| | | | '_ \ / _ \ '__|   | |/ _` \ \/ / | |   / _` \ \ /\ / / | | |/ _ \ '__|
+                  | ___) | |_| | |_) |  __/ |      | | (_| |>  <  | |__| (_| |\ V  V /| |_| |  __/ |
+                  ||____/ \__,_| .__/ \___|_|      |_|\__,_/_/\_\ |_____\__,_| \_/\_/  \__, |\___|_|
+                  |            |_|                                                     |___/
+                  |		""".stripMargin + s"""
+
+    Super Tax Lawyer is a program to play with accounting exported as text files.
+     """)
+    val fileExist: String => Boolean = new File(_).exists()
+    val fileListExist: List[String] => Boolean = _.forall(fileExist)
+
+    val columnSize = opt[String]("columnSize", descr = "Print the detected encoding of each file provided.", validate = fileExist)
+    val splitter = opt[String]("splitter", descr = "Character used to split a line in columns. Use TAB for tabulation and SPACE for space separators.")
+    val encoding = opt[String]("forceEncoding", descr = "Force the encoding of the text file.")
+    val columnCount = opt[Int]("columnCount", descr = "[OPTIONAL] Number of columns expected.")
+    val excludeTitles = toggle("excludeTitles", descrYes = "Exclude titles of columns in column size result.", default = Some(false), prefix = "no-")
+    val output = opt[String]("outputFolder", descr = "Path to the folder where to save the results.", validate = new File(_).isDirectory)
+    val debug = toggle("debug", descrYes = "Display lots of debug information during the process.", descrNo = "Display minimum during the process (same as not using this argument).", default = Some(false), prefix = "no-")
+    val help = opt[Boolean]("help", descr = "Show this message.")
+    codependent(columnSize, splitter)
+    conflicts(columnSize, List(help))
+}
--- src/test/scala/com.taj.supertaxlawyer.test/CounterTest.scala
+++ src/test/scala/com.taj.supertaxlawyer.test/CounterTest.scala
@@ -37,6 +37,7 @@ import com.taj.supertaxlawyer.FileStructure._
 import com.taj.supertaxlawyer.Distributor
 import com.taj.supertaxlawyer.ActorMessages.Start
 import com.typesafe.scalalogging.slf4j.Logging
+import java.nio.charset.StandardCharsets
 
 
 case class testContainer(name: String, numberOfColumns: Int, columnCountWithTitles: List[Int], columnCountWithoutTitles: List[Int], splitter: String, encoding: String, numberOfLines: Long)
@@ -54,6 +55,7 @@ class CounterTest extends TestKit(ActorSystem("AkkaTestSystem")) with ImplicitSe
   val pipe = testContainer("pipe.csv", 10, List(7, 7, 29, 7, 32, 7, 7, 7, 7, 8), List(4, 7, 29, 5, 32, 4, 5, 4, 4, 4), "|", "ISO-8859-2", 25l)
   val semicolon_with_title = testContainer("semicolon_with_document_title_on_one_column.csv", 10, List(7, 7, 29, 7, 32, 7, 7, 7, 7, 8), List(7, 7, 29, 7, 32, 7, 7, 7, 7, 8), ";", "ISO-8859-2", 26l)
   val tab = testContainer("tab.txt", 10, List(7, 7, 9, 7, 7, 7, 15, 7, 7, 20), List(4, 7, 9, 5, 4, 4, 15, 4, 4, 20), "\t", "ISO-8859-2", 24l)
+  val utf8 = testContainer("utf8_file.txt", 3, List(24, 23, 27), List(24, 23, 23), ";", "ISO-8859-1", 2)
 
   /**
    * Clean all temp files before starting
@@ -63,16 +65,25 @@ class CounterTest extends TestKit(ActorSystem("AkkaTestSystem")) with ImplicitSe
   }
 
   Seq(semicolon, semicolon_with_title, tab, pipe)
-    .foreach {
+  .foreach {
     fileToTest =>
-      s"We will evaluate the column sizes of the file ${fileToTest.name}." must {
+      s"We will evaluate the encoding of the file ${fileToTest.name}." must {
         val file = new File(encodedFileFolder, fileToTest.name)
-        val numberOfColumns = FileSizeTools.columnCount(file.getAbsolutePath, fileToTest.splitter, fileToTest.encoding)
 
         s"The encoding should be detected as ${fileToTest.encoding}" in {
           val encoding = FileSizeTools.detectEncoding(file.getAbsolutePath)
           encoding should equal(fileToTest.encoding)
         }
+      }
+  }
+
+
+  Seq(semicolon, semicolon_with_title, tab, pipe, utf8)
+    .foreach {
+    fileToTest =>
+      s"We will evaluate the column sizes of the file ${fileToTest.name}." must {
+        val file = new File(encodedFileFolder, fileToTest.name)
+        val numberOfColumns = FileSizeTools.columnCount(file.getAbsolutePath, fileToTest.splitter, fileToTest.encoding)
 
         s"The number of columns should be ${fileToTest.numberOfColumns}" in {
           numberOfColumns should equal(fileToTest.numberOfColumns)
