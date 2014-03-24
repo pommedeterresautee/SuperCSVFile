769321dd3848f0956b3978b3c177ce056a4944e1
El Potaeto <contact@2borange.com>
2014-03-24 16:22:27 +0100

    add column size tests for a new UTF-8 files.

--- src/test/scala/com/taj/supertaxlawyer/test/DataToTest.scala
+++ src/test/scala/com/taj/supertaxlawyer/test/DataToTest.scala
@@ -12,5 +12,6 @@ object DataToTest {
   val pipe = TestContainer("pipe.csv", 10, List(7, 7, 29, 7, 32, 7, 7, 7, 7, 8), List(5, 7, 29, 5, 32, 4, 5, 4, 4, 4), "|", "ISO-8859-2", 25l)
   val semicolon_with_title = TestContainer("semicolon_with_document_title_on_one_column.csv", 10, List(7, 7, 29, 7, 32, 7, 7, 7, 7, 8), List(7, 7, 29, 7, 32, 7, 7, 7, 7, 8), ";", "ISO-8859-2", 26l)
   val tab = TestContainer("tab.txt", 10, List(7, 7, 9, 7, 7, 7, 15, 7, 7, 20), List(4, 7, 9, 5, 4, 4, 15, 4, 4, 20), "\t", "ISO-8859-2", 24l)
-  val fake_utf8 = TestContainer("utf8_file.txt", 3, List(24, 23, 27), List(24, 23, 23), ";", "ISO-8859-1", 2)
+  val utf8 = TestContainer("utf8_file_bis.txt", 3, List(3, 16, 10, 8, 10, 50, 10, 20, 14, 8, 12, 9, 9, 11, 8, 8), List(3, 16, 10, 8, 10, 50, 1, 1, 14, 8, 12, 9, 9, 11, 8, 8), "\t", "UTF-8", 5)
+  val fake_utf8_bis = TestContainer("utf8_file.txt", 3, List(24, 23, 27), List(24, 23, 23), ";", "ISO-8859-1", 2)
 }
