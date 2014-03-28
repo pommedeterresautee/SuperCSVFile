dc2ca0e9cb4a2689820a2d82472b555ab5e00c81
El Potaeto <contact@2borange.com>
2014-03-28 18:34:52 +0100

    Re add forgotten push

--- src/main/scala/com/taj/supertaxlawyer/Main.scala
+++ src/main/scala/com/taj/supertaxlawyer/Main.scala
@@ -46,6 +46,7 @@ object Main extends App {
   val file2UTF8 = encodedFileFolder + "utf8_file_bis.txt"
   val argUTF8 = Array("--columnSize", fileUTF8, "--forceEncoding", "ISO-8859-1", "--excludeTitles")
   val arg = Array("--columnSize", file)
+  val argExtract = Array("--extractLines", file, "--firstLine", "0", "--lastLine", "0")
 
   ExecuteCommandLine(args)
 }
